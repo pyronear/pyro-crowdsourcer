1dcb9cbdd7055e756125d1087ed68605986a5ce6
fe51 <55736935+fe51@users.noreply.github.com>
2022-02-12 01:38:12 +0100

    Add "Legal" justification of the collection process - Update Design - Translation (#10)

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@
 
 import time
 from datetime import date
+from pathlib import Path
 
 import streamlit as st
 
@@ -19,50 +20,67 @@ def main():
     )
 
     # Designing the interface
-    st.title("Wildfire image crowdsourcing")
-    st.write("The image you upload here will be used to create a public image dataset for wildfire detection.")
+    st.title("Améliorez la detection des départs de feux")
+    st.write("La photo que vous partagez ici sera utilisée pour créer un jeu de données public "
+             "pour la detection de feux de forêts.")
+    st.write("Idéalement partagez nous une photo depuis une vue assez dégagée sur une zone forestière,"
+             " avec et/ou sans flammes/fumées")
+
     # Set the columns
     cols = st.columns((2, 1))
-    cols[0].subheader("Input image")
-    cols[1].subheader("Image tags")
+    cols[0].subheader("Sélection de la photo")
+    cols[1].subheader("Informations de la photo")
 
     # Sidebar
-    # File selection
-    st.sidebar.title("Image selection")
+    st.sidebar.image("src/static/logo_letters.png")
+
+    # new line
+    st.sidebar.write('\n')
+
+    st.sidebar.markdown("""**A propos de Pyronear** - Pyronear est une association loi 1901, uniquement constituée \
+    de bénévoles, qui a pour objet la préservation des espaces naturels contre les risques d’incendies, \
+    et autres aléas naturels. \n \n Pour cela, nous nous efforçons de trouver des solutions technologiques \
+    performantes, accessibles, open-source et abordables pour la protection contre les risques naturels. """)
+
+    st.sidebar.write('\n')
+    st.sidebar.markdown("""[Site Web](https://pyronear.org/) | [contact](mailto:contact@pyronear.org)""")
+
     # Disabling warning
     st.set_option('deprecation.showfileUploaderEncoding', False)
+
     # Choose your own image
-    uploaded_file = st.sidebar.file_uploader("Upload files", type=['png', 'jpeg', 'jpg'])
+    uploaded_file = cols[0].file_uploader("Upload files", type=['png', 'jpeg', 'jpg'])
     submitted = False
     if uploaded_file is not None:
         st.warning(
-            "By clicking the 'Submit' button, you hereby declare that you are authorized to share this picture "
-            "and forsake all the rights of exclusive exploitation of this content."
+            """En partageant cette photo, vous adhérez à la démarche Pyronear """
+            """de construction d'un jeu de données collaboratif."""
         )
         img = uploaded_file.read()
         cols[0].image(img)
-        form = cols[1].form("Image information")
-        form.date_input("Date of the picture", max_value=date.today())
-        form.time_input("Approx time of the picture")
-        form.text_input("Where was this taken?")
-        form.multiselect("Event tags", ["smoke", "flames"])
-        submitted = form.form_submit_button("Submit")
+        form = cols[1].form("Information de la photo")
+        form.date_input("Date de la photo", max_value=date.today())
+        form.time_input("Heure approximative")
+        form.text_input("Où a-t-elle été prise ?")
+        form.multiselect("Étiquettes", ["fumée", "flammes"])
+        submitted = form.form_submit_button("Envoyer")
 
-    # For newline
-    st.sidebar.write('\n')
+    # Find out more section
+    with st.expander("En savoir plus sur notre démarche de construction d'un dataset collaboratif"):
+        st.markdown(Path("/src/static/find-out-more.md").read_text())
 
     if submitted:
 
         if uploaded_file is None:
-            st.sidebar.write("Please upload an image")
+            cols[0].write("Veuillez charger une photo")
 
         else:
 
-            with st.spinner('Uploading...'):
+            with st.spinner('Envoi en cours ...'):
 
                 # Call API to send image + labels
                 time.sleep(.5)
-            st.success('Thanks for your upload!')
+            st.success("Merci pour l'envoi !")
             st.balloons()
 
 
---
+++ src/static/find-out-more.md
@@ -0,0 +1,65 @@
+#### Qui sommes-nous ?
+
+Pyronear est une association loi 1901, uniquement constituée de bénévoles, qui a pour objet la préservation 
+des espaces naturels contre les risques d’incendies, et autres aléas naturels.
+
+Pour cela, nous nous efforçons de trouver des solutions technologiques performantes, accessibles, open-source 
+et abordables pour la protection contre les risques naturels.
+
+#### Pourquoi cette collecte participative de photos de feux de forêts ?
+
+Nous souhaitons permettre davantage d’implication citoyenne dans notre projet. 
+
+Un élément essentiel de notre solution de détection précoce de départ de feux, c’est un algorithme de deep learning 
+(vous pouvez l’appeler Intelligence artificielle, certains trouvent que c’est plus à la mode), 
+capable de discerner un départ de feux sur une image. 
+Plus cet algorithme aura vu d’images avec une vue surplombante sur la forêt, 
+avec et sans départs de feux,
+dans des situations différentes, meilleur il sera. 
+
+Par conséquent, partager des images de forêt avec ou sans fumées/flammes, c’est nous aider 
+et contribuer à la préservation des espaces naturels. Merci beaucoup !
+
+#### Que se passe-t-il quand vous partagez une photo sur notre site ?
+Nous collectons uniquement les images pertinentes relatives à des départs de feux, 
+et les informations descriptives associées : date, heure approximative, département, tags. 
+
+C’est pourquoi nous vous demandons de ne partager que des images en lien avec cet objectif. 
+Nous ne voulons en aucun cas traiter des informations personnelles 
+(par exemple une photo avec le visage d’une personne).
+
+#### Comment les images sont-elles triées ?
+Nous ne gardons que les photos pertinentes au regard de nos objectifs et effectuons d’ailleurs un tri automatisé 
+des images afin de ne garder que les pertinentes. 
+
+Nous nous réservons le droit de ne pas conserver les images partagées.
+
+#### A quoi vont servir les informations collectées ?
+L’objectif de cette application de collecte collaborative de photos de feux de forêt, est de constituer 
+un jeu de données open data de qualité pour détecter de façon précoce les départs de feux de forêts.
+
+#### Le code source de l'application est-il disponible ?
+Comme tous nos travaux, nous partageons nos codes en open source. 
+
+Celui de cette application se trouve juste ici.
+
+#### Envie de contribuer ou d'en savoir davantage ?
+
+Toute énergie positive est la bienvenue pour nous aider à faire avancer ce projet.
+
+N’hésitez pas à nous contacter sur les réseaux ou par mail à [contact@pyronear.org](mailto:contact@pyronear.org).
+
+[linkedin](https://www.linkedin.com/company/pyronearfr) | [github]( https://github.com/pyronear) | 
+[twitter](https://twitter.com/pyro_near) | [facebook](https://www.facebook.com/pyronear)
+
+#### Qu'est ce qui nous motive ?
+
+A ce jour, l’ère de l’anthropocène est marquée par une pression très forte exercée par l’homme sur l’environnement. 
+Les changements climatiques actuels en sont le reflet, le dernier rapport du GIEC confirmant l’accentuation des 
+phénomènes météorologiques et climatiques extrêmes augmentant l’intensité et les surfaces forestières soumises 
+aux risques d’incendies.
+
+ Notre conviction est que les développements technologiques récents, utilisés à bon escient et de façon raisonnée, 
+peuvent soutenir la cohabitation entre l’homme et son habitat naturel. 
+
+C’est donc une opportunité d’embarquer dans une aventure associative au service du bien commun.
