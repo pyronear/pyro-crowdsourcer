cae73ef78a2cbcf2b2754f14669fd01192429b02
Paul-Emmanuel Chouc <pechouc@gmail.com>
2022-06-01 22:00:22 +0200

    isort fix

--- src/app.py
+++ src/app.py
@@ -1,19 +1,14 @@
-import pandas as pd
-from datetime import datetime, date
+from datetime import date, datetime
 
 import dash
-
-from dash.dependencies import Input, Output, State
-from dash.exceptions import PreventUpdate
-
-# Various modules provided by Dash and Dash Leaflet to build the page layout
-from dash import dcc
-from dash import html
 import dash_bootstrap_components as dbc
 import dash_daq as daq
-
+import pandas as pd
+# Various modules provided by Dash and Dash Leaflet to build the page layout
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 from user_agents import parse
-
 
 # Fetching French departements
 departments = pd.read_csv(
