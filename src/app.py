9356c7113b787a9960d4ac69dde1a4d84476f24f
Paul-Emmanuel Chouc <pechouc@gmail.com>
2022-06-13 18:09:11 +0200

    Removing unused import

--- src/app.py
+++ src/app.py
@@ -10,7 +10,6 @@ from datetime import date, datetime
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_daq as daq
 import dash_mantine_components as dmc
 # import dash_uploader as du
 import requests
